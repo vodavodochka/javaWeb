8f94355b784ca11c3b2a4391525eb51fa7841b10
vodavodochka <msadunov@gmail.com>
2025-02-10 15:00:40 +0500

    thymeleaf

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
 
     @ResponseBody
